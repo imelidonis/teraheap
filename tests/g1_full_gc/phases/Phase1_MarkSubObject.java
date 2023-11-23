ba616feae0b03976daf07317aa0584c2605f5b73
Ioannis Melidonis <melidonis00@gmail.com>
2023-11-23 17:36:41 +0200

    [test] Change array sizes

--- tests/g1_full_gc/phases/Phase1_MarkSubObject.java
+++ tests/g1_full_gc/phases/Phase1_MarkSubObject.java
@@ -21,13 +21,13 @@ public class Phase1_MarkSubObject {
   public static void main(String[] args) {
     // Create an ArrayList of Arrays of class A
     ArrayList<A[]> arraysOfA = new ArrayList<A[]>();
-    // Create an array of 10 objects of class A
-    A[] arrayOfA = new A[10];
-    // Create an array of 10 object of class A what will not be marked.
-    A[] arrayOfA_nomove = new A[10];
+    // Create an array of 2048 objects of class A
+    A[] arrayOfA = new A[2048];
+    // Create an array of 2048 object of class A what will not be marked.
+    A[] arrayOfA_nomove = new A[2048];
 
-    // Instantiate 10 objects of class A and assign them to the array
-    for (int i = 0; i < 10; i++) {
+    // Instantiate 2048 objects of class A and assign them to the array
+    for (int i = 0; i < 2048; i++) {
       arrayOfA[i] = new A();
       arrayOfA_nomove[i] = new A();
     }
@@ -51,7 +51,7 @@ public class Phase1_MarkSubObject {
 
     // Check that all objects are marked of arrayOfA are marked,
     // and objects of arrayOfA_nomove are not!
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 2048; i++) {
       if (!(_UNSAFE.is_marked_move_h2(arrayOfA[i])))
         throw new RuntimeException("Object 'arrayOfA[" + i + "]' should be marked!");
       if (_UNSAFE.is_marked_move_h2(arrayOfA_nomove[i]))

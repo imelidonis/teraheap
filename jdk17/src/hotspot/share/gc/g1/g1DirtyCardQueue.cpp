75ea6eb27ecbf256b99dc687659dcd526246b86c
mairi ch <marycharalambous.uni@gmail.com>
2024-01-03 14:55:19 +0200

    refinement log extended

--- jdk17/src/hotspot/share/gc/g1/g1DirtyCardQueue.cpp
+++ jdk17/src/hotspot/share/gc/g1/g1DirtyCardQueue.cpp
@@ -513,6 +513,10 @@ void G1DirtyCardQueueSet::handle_completed_buffer(BufferNode* new_node,
     return;
   }
 
+#ifdef REFINE_LOG
+  double _vtime_start = os::elapsedVTime();
+#endif
+
   BufferNode* node = get_completed_buffer();
   if (node == NULL) return;     // Didn't get a buffer to process.
 
@@ -524,6 +528,10 @@ void G1DirtyCardQueueSet::handle_completed_buffer(BufferNode* new_node,
 
   // Deal with buffer after releasing id, to let another thread use id.
   handle_refined_buffer(node, fully_processed);
+
+#ifdef REFINE_LOG
+  stats->inc_refinement_vtime(os::elapsedVTime() - _vtime_start);
+#endif
 }
 
 bool G1DirtyCardQueueSet::refine_completed_buffer_concurrently(uint worker_id,
